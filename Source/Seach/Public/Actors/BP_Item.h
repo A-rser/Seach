a3e497eba6e744fe700ea839e5d456f4439806b2
JiangShouYun <2233643450@qq.com>
2026-01-21 15:34:10 +0800

    21

--- Source/Seach/Public/Actors/BP_Item.h
+++ Source/Seach/Public/Actors/BP_Item.h
@@ -21,7 +21,7 @@ protected:
 
 	UFUNCTION()
 	void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
-
+	void EndSphereOverlap()
 public:
 	virtual void Tick(float DeltaTime) override;
 
