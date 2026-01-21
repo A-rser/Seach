ac11e858afc20bf21a11649f0c7a4c945969669b
JiangShouYun <2233643450@qq.com>
2026-01-21 17:55:32 +0800

    创建武器父类

--- Source/Seach/Private/Actors/Item.cpp
+++ Source/Seach/Private/Actors/Item.cpp
@@ -1,16 +1,19 @@
 	// Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Actors/BP_Item.h"
+#include "Actors/Item.h"
 #include "Components/SphereComponent.h"
+#include "Components/StaticMeshComponent.h"
 
 // Sets default values
 AItem::AItem()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	RootMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ItemMesh"));
+	RootComponent = RootMesh;
 	Sphere = CreateDefaultSubobject<USphereComponent>(TEXT("Sphere"));
-	Sphere->SetupAttachment(GetRootComponent());
+	Sphere->SetupAttachment(RootMesh);
+	PrimaryActorTick.bCanEverTick = true;
 }
 
 // Called when the game starts or when spawned
---
+++ Source/Seach/Public/Actors/Item.h
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Item.generated.h" 
+
+// ǰ��������÷������UCLASS ������
+class USphereComponent;
+class UStaticMeshComponent;
+
+
+// ���޸��ص㡿������ֻ��Ҫһ�� UCLASS�����Ҽ��� Blueprintable
+UCLASS(Blueprintable)
+class SEACH_API AItem : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AItem();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	virtual void OnSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	virtual void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	USphereComponent* Sphere;
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* RootMesh;
+
+};
